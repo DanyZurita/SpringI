862fc18374101c4a7294f8b43cb344821066a2e6
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-05 11:23:31 +0200

    fix(ServeiAlumnat): Añadir logica a matricula() para el caso test

--- src/main/java/org/formacio/component/ServeiAlumnat.java
+++ src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -1,11 +1,13 @@
 package org.formacio.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServeiAlumnat {
 
-	
+	@Autowired
+	private RepositoriAlumnesMemoria bbdd;
 	/**
 	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
 	 * el corresponent codi.
@@ -13,7 +15,13 @@ public class ServeiAlumnat {
 	 * Retorna true si l'alumne s'ha inserit, false si no.
 	 */
 	public boolean matricula (int id, String alumne) {
-		return true;
+		if (alumne != null) {
+			bbdd.altaAlumne(id, alumne);
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 }
